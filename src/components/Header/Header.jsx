534e22afacdf6befe8994b60de41c7967254553d
Tigran <41443306+septoon@users.noreply.github.com>
2021-01-22 17:51:10 +0200

    mobile header refresh

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,9 @@ const Header = (props) => {
             <img src={logo} alt="logo" className={classes.logo} />
             <div className={classes.contacts}>
                <div className={classes.cotacts} >
-                <span>Наш адрес: ул. Ленина 1</span>
-                <span>Наш телефон: <a href="tel: +79785068725" className={classes.num}>+7 (978) 506-87-25</a></span>
+                <span className={classes.contact}>Наш адрес: ул. Ленина 1</span>
+                <span className={classes.contact}>Наш телефон:</span>
+                 <a href="tel: +79785068725" className={classes.num}>+7 (978) 506-87-25</a>
                </div>
             </div>
         </div>
